8a7864f1218cb5e3f878f9886e49efd6bf3be684
abdallah moumni <abdelrebel001@gmail.com>
2023-10-10 09:24:14 -0700

    commit_1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
+/**
+ * main - Prints Holberton as a message.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{ printf ("_putchar\n");
-  return 0;
+{
+	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
 }
