6fc1ff1e00895d43e7db5716aef4876fefb036c1
abdallah moumni <abdelrebel001@gmail.com>
2023-10-10 10:10:59 -0700

    commit_2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+    int a,b;
+	
+	for (a=0, a<=9 ,i++);
+	{
+	    for (b=97, b<=122; i++)
+		{
+		_putchar(b);
+		}
+	_putchar("\n");
+	}
+}
