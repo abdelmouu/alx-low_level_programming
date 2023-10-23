0c61730f25e6402ffee9472200498b4b3901e707
abdallah moumni <abdelrebel001@gmail.com>
2023-10-23 09:59:03 -0700

    commit_n2

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,8 +8,10 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n);
 {
-	for (unsigned int i=0, i < n, i++)
-	            *(dest + i) =  *(src + i);
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) =  *(src + i);
 				
 	return (dest);
 }
