a7c85beac5b91e91ba5919080adec8a70001121d
abdallah moumni <abdelrebel001@gmail.com>
2023-10-23 09:56:45 -0700

    commit_n2

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _memcpy function, which takes three arguments.
+ * dest: pointer to the destination memory erea where the data will be copied.
+ * src: pointer to the source memory area where the data will be copied.
+ * n: an unsugnerd int specifying the nmbr of bytes to copy 
+ * Return: the pointer to dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	for (unsigned int i=0, i < n, i++)
+	            *(dest + i) =  *(src + i);
+				
+	return (dest);
+}
