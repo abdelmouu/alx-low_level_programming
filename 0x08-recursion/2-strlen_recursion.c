9b2c0c761b6d78f1fab2ac340980a12991abae38
abdallah moumni <abdelrebel001@gmail.com>
2023-10-25 09:28:50 -0700

    commit_n2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - number of characters in a string
+ * @s: string
+ * Return: lenght of string.
+ */
+int _strlen_recursion(char *s);
+{
+	if (*s == '\0')
+		return (o);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
