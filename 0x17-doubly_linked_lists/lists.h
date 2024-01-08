3145950320c250beb0ee56002e4231e2bb80e87d
abdallah moumni <abdelrebel001@gmail.com>
2024-01-08 10:17:23 -0800

    ___________________________________________

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer (data)
+ * @prev: points to the previous node in the list
+ * @next: points to the next node in the list
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+/* Function prototype */
+size_t print_dlistint(const dlistint_t *h);
+
+#endif /* LISTS_H */
