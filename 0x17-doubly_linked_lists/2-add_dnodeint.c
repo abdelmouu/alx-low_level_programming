7ef436b4c3d2554142ab10df0a348da55ca10d47
abdallah moumni <abdelrebel001@gmail.com>
2024-01-11 17:50:14 -0800

    ++++++++++++++++++++++++++++++++++++++

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,41 +1,23 @@
 #include "lists.h"
-
 /**
- * add_dnodeint - adds a new node with n to the list's
- * beginning
- *
- * @head: the head of the list
- * @n: the value of the element
- * Return: address of the new element
- */
-
+  * add_dnodeint - Adds 1 node to the beging of a D-L-L
+  *
+  * @head: the head of the linked list
+  * @n: the value to add to the list
+  *
+  * Return: pointer to the element added
+  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = NULL;
+	dlistint_t *tmp;
 
-	if (head == NULL)
+	tmp = *head;
+	tmp = malloc(sizeof(dlistint_t));
+	if (tmp == NULL)
 		return (NULL);
-
-	new_node = malloc(sizeof(dlistint_t));
-
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-
-	if (*head == NULL)
-	{
-		new_node->next = NULL;
-		new_node->prev = NULL;
-		*head = new_node;
-		return (*head);
-	}
-
-
-	new_node->next = *head;
-	new_node->prev = NULL;
-	(*head)->prev = new_node;
-	*head = new_node;
-
-	return (new_node);
+	tmp->n = n;
+	tmp->prev = NULL;
+	tmp->next = *head;
+	*head = tmp;
+	return (tmp);
 }
